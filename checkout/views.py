fbd5cef97733cad3df22eb5705720bea5e66f691
Nathan Drake <nathan@mail-hub.co.uk>
2024-03-13 13:39:13 +0000

    Add: Stripe card element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OtrlTJhq7EQqQ72MEUHGUrHC97QxReXSPT9ac2cdr1RDHHQyfz17WpHJlXkbCaV9vqizTUBQ3UhhiCiiVmDfRMf00B8A7cGG9',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
